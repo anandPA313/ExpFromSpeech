660c02b925b869c2464d6a2c511ac32dfe1908c4
Anand Ashtamoorthy <paanand.3@gmail.com>
2017-03-23 01:13:35 +0530

    Update seeSpectrum.py
    Function to plot DFT

--- seeSpectrum.py
+++ seeSpectrum.py
@@ -1,15 +1,29 @@
-import import scipy.fftpack
+import scipy.fftpack
 import matplotlib.pyplot as plt
 
+# Function to plot FFT of any signal time domain.
+
 def seeSpectrum(data, fs):
+# Signal - the time domain sequence
+# fs     - the sampling frequency of the signal
+    
     n = len(signal)
     k = np.arange(n)
     T = float(n)/fs
-    frq = k/T
+    
+    # Get the frequency values
+    frq = k/T       
+    
+    # We want only real spectrum
     frq = frq[range(n/2)]
+    
+    # Compute normalized FFT
     Y = np.fft.rfft(signal)/n
+    
+    # Real part
     Y = Y[range(n/2)]
     
+    # Plot and see
     plt.plot(frq, Y)
     plt.xlabel('Frequency in Hz')
     plt.ylabel('Normalized spectrum')
